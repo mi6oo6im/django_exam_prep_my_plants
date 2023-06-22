1d5b7001a7042a0cba801e003a7e51025b865e91
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-06-22 20:28:07 +0100

    context processor added

--- django_exam_prep_my_plants/settings.py
+++ django_exam_prep_my_plants/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django_exam_prep_my_plants.processors.get_profile',
             ],
         },
     },
@@ -76,23 +77,17 @@ WSGI_APPLICATION = 'django_exam_prep_my_plants.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-#
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "my_plants",
-#         "USER": "postgres_user",
-#         "PASSWORD": "pass123",
-#         "HOST": "127.0.0.1",
-#         "PORT": "5432",
-#     }
-# }
+
+ DATABASES = {
+     "default": {
+         "ENGINE": "django.db.backends.postgresql",
+         "NAME": "my_plants",
+         "USER": "postgres_user",
+         "PASSWORD": "pass123",
+         "HOST": "127.0.0.1",
+         "PORT": "5432",
+     }
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
